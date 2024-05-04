cf7d388dc33a50f7c7abe2153cf80b897079d2cd
salam99823 <salamatbekboev2008@gmail.com>
2024-05-04 16:16:33 +0900

    -

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,28 +3,75 @@ require "utils/aliases"
 return {
     {
         "nvim-lualine/lualine.nvim",
-        dependencies = { 
+        dependencies = {
             'nvim-tree/nvim-web-devicons',
         },
         config = function()
-            require "plug/lualine"
+            require "plug.lualine"
         end
     },
 
     {
         "tanvirtin/monokai.nvim",
         config = function()
-            require "plug/monokai"
+            require "plug.monokai"
         end
     },
-    
+
     {
         "loctvl842/monokai-pro.nvim",
         dependencies = {
             'MunifTanjim/nui.nvim',
         },
         config = function()
-            require "plug/monokai-pro"
+            require "plug.monokai-pro"
         end
+    },
+
+    {
+        "williamboman/mason.nvim",
+        dependencies = {
+            {
+                "williamboman/mason-lspconfig.nvim", -- LSP
+                dependencies = {
+                    "neovim/nvim-lspconfig",
+                },
+                config = function()
+                    require "plug.lspconfig"
+                end
+            },
+
+            {
+                "rcarriga/nvim-dap-ui", -- DAP
+                dependencies = {
+                    "mfussenegger/nvim-dap",
+                    "nvim-neotest/nvim-nio",
+                },
+                config = function()
+                    require "plug.dap"
+                end
+            },
+
+            {
+                "mfussenegger/nvim-lint", -- Linter
+                config = function()
+                    require "plug.lint"
+                end
+            },
+
+            {
+                "mhartington/formatter.nvim", -- Formatter
+                config = function()
+                    require "plug.formatter"
+                end
+            }
+        },
+        config = function()
+            require "plug.mason"
+        end
+    },
+
+    {
+        ""
     },
 }
