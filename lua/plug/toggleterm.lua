7a02eb3552aa463921aa5e5b0a9ad78ddd474f35
salam99823 <salamatbekboev2008@gmail.com>
2024-05-09 23:30:30 +0900

    -

--- lua/plug/toggleterm.lua
+++ lua/plug/toggleterm.lua
@@ -5,4 +5,8 @@
 ]]
 require("toggleterm").setup({
 	open_mapping = "<c-\\>",
+	close_on_exit = true, -- close the terminal window when the process exits
+	-- Change the default shell. Can be a string or a function returning a string
+	shell = vim.o.shell,
+	auto_scroll = true,
 })
