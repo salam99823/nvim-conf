e30f53960ebf2fc8ffaaaefbc275f4b42082be90
salam99823 <salamatbekboev2008@gmail.com>
2024-05-09 23:18:05 +0900

    -

---
+++ lua/plug/toggleterm.lua
@@ -0,0 +1,8 @@
+--[[
+    File: toggleterm.lua
+    Description: Configuration of toggleterm.nvim
+    See: https://github.com/akinsho/toggleterm.nvim
+]]
+require("toggleterm").setup({
+	open_mapping = "<c-\\>",
+})
