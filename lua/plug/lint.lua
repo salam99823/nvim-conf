c454b092c7370c7726182ffa1b42303fcd5929c8
salam99823 <salamatbekboev2008@gmail.com>
2024-05-09 19:46:00 +0900

    -

--- lua/plug/lint.lua
+++ lua/plug/lint.lua
@@ -3,3 +3,7 @@
     Description: Formatter plugin configuration
     See: http://github.com/mfussenegger/nvim-lint
 ]]
+require("lint").linters_by_ft = {
+	markdown = { "markuplint" },
+	python = { "ruff", "mypy" },
+}
