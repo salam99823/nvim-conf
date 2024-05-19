58463abd2cdb9577eb49651a3588ab4a3f739c60
salam99823 <salamatbekboev2008@gmail.com>
2024-05-19 19:40:52 +0900

    изменено:      lua/plug/formatter.lua

--- lua/plug/formatter.lua
+++ lua/plug/formatter.lua
@@ -5,29 +5,45 @@
 ]]
 local util = require("formatter.util")
 require("formatter").setup({
-	-- Enable or disable logging
 	logging = true,
-	-- Set the log level
 	log_level = vim.log.levels.WARN,
-	-- All formatter configurations are opt-in
 	filetype = {
-		lua = { require("formatter.filetypes.lua").stylua },
+		lua = {
+			require("formatter.filetypes.lua").stylua(),
+		},
 		python = {
-			{
-				exe = "ruff",
-				args = {
-					"format",
-					util.escape_path(util.get_current_buffer_file_path()),
-				},
+			require("formatter.filetypes.python").ruff(),
+		},
+		rust = {
+			exe = "rustfmt",
+			args = {
+				util.escape_path(util.get_current_buffer_file_path()),
 			},
 		},
-		rust = require("formatter.filetypes.rust"),
 		markdown = {
 			{
 				exe = "markdownlint",
 				args = {
 					util.escape_path(util.get_current_buffer_file_path()),
 				},
+			},
+		},
+    svelte = {
+      require("formatter.filetypes.svelte").prettier
+    },
+		css = {
+			require("formatter.filetypes.css").prettier,
+		},
+    toml = {
+      require("formatter.filetypes.toml").taplo,
+    },
+		pest = {
+			{
+				exe = "pestfmt",
+				args = {
+					"--stdin",
+				},
+				stdin = true,
 			},
 		},
 		["*"] = require("formatter.filetypes.any"),
